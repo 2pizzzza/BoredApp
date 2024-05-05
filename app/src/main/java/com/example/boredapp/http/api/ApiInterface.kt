d53d594faf1e527d7e16546a918f5940b5d9a26a
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-05-05 16:39:31 +0600

    setup retrofit and added endpoint

--- app/src/main/java/com/example/boredapp/http/api/ApiInterface.kt
+++ app/src/main/java/com/example/boredapp/http/api/ApiInterface.kt
@@ -1,4 +1,10 @@
 package com.example.boredapp.http.api
 
+import com.example.boredapp.http.response.Activity
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface ApiInterface {
+    @GET("activity")
+    fun getActivity() : Call<Activity>
 }
