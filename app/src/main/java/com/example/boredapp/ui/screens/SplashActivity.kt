0d8528bc1b59da48e34dc72c34c00905570e8b2c
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-05-05 18:05:10 +0600

    added pager in splash activity

--- app/src/main/java/com/example/boredapp/ui/screens/SplashActivity.kt
+++ app/src/main/java/com/example/boredapp/ui/screens/SplashActivity.kt
@@ -5,9 +5,16 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
@@ -33,25 +40,42 @@ class SplashActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalFoundationApi::class)
     @Preview(showSystemUi = true)
     @Composable
     private fun SplashScreen() {
-        LaunchedEffect(key1 = true) {
-            delay(2000)
-            startActivity(Intent(this@SplashActivity, MainActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-            })
-            finish()
-        }
+        val pageState = rememberPagerState(pageCount = { 3 })
+//        LaunchedEffect(key1 = true) {
+//            startActivity(Intent(this@SplashActivity, MainActivity::class.java).apply {
+//                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+//            })
+//            finish()
+//        }
         Box(
             contentAlignment = Alignment.Center,
             modifier = Modifier.fillMaxSize()
         ) {
-            Image(
-                contentScale = ContentScale.Crop,
-                painter = painterResource(id = R.drawable.splash_dog),
-                contentDescription = "splash_img"
-            )
+            HorizontalPager(state = pageState) { page ->
+                Column {
+                    Text(
+                        text = "$page, pagwer",
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    if (page == 2) {
+                        Button(onClick = {
+                            startActivity(Intent(
+                                this@SplashActivity,
+                                MainActivity::class.java).apply {
+                                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                            })
+                            finish()
+                        }) {
+                            Text(text = "i am spider man")
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
