136795800fda6294b56846efecb7959580886ac2
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-05-05 16:32:16 +0600

    first setup project

---
+++ app/src/main/java/com/example/boredapp/MainViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.boredapp
+
+class MainViewModel {
+}
---
+++ app/src/main/java/com/example/boredapp/ui/screens/MainScreen.kt
@@ -0,0 +1,6 @@
+package com.example.boredapp.ui.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen(){}
